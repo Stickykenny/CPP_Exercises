85ed9ac39aabe582dbcf1c5c9f336f162d7eb038
Jérémy VONG <gnov.jeremy@gmail.com>
2023-01-12 14:39:34 +0100

    Adding hello world program.

---
+++ TP1/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	auto tmp = 0;
+	std::cout << "Hello World !" << std::endl;
+	std::cin >> tmp; // input inutile pour ne pas que la fenêtre se ferme directement
+	return 0;
+}
