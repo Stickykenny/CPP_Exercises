defcfe66752ad334834023220e3ddc8dd5ffe3d4
Jérémy VONG <gnov.jeremy@gmail.com>
2023-01-24 11:39:32 +0100

    tmpsave

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -2,4 +2,25 @@
 
 class Plush
 {
+public:
+    Plush() {} // pas besoin y'a déjà
+    Plush(unsigned int cost)
+
+        : _cost{
+              cost}
+    {
+    }
+
+    unsigned int get_cost()
+        const
+    {
+        return _cost;
+    }
+    void set_cost(int new_cost)
+    {
+        _cost = new_cost;
+    }
+
+private:
+    unsigned int _cost = 10u;
 };
--- tests/test-00.cpp
+++ tests/test-00.cpp
@@ -5,16 +5,16 @@
 TEST_CASE("Test 00 - "
           "1. Une string instanciee par defaut a pour taille 0.")
 {
-    const auto default_string = std::string {};
+    const auto default_string = std::string{};
     REQUIRE(default_string.length() == 0);
 }
 
 TEST_CASE("Test 00 - "
           "2. Deux string peuvent etre concatenees avec +.")
 {
-    const auto empty_string = std::string {};
-    const auto str_a        = std::string { "toto" };
-    const auto str_b        = std::string { "turlututu" };
+    const auto empty_string = std::string{};
+    const auto str_a = std::string{"toto"};
+    const auto str_b = std::string{"turlututu"};
     REQUIRE(empty_string + str_a == "toto");
     REQUIRE(str_b + empty_string == "turlututu");
     REQUIRE(str_a + str_b == "tototurlututu");
@@ -24,8 +24,8 @@ TEST_CASE("Test 00 - "
 TEST_CASE("Test 00 - "
           "3. Une string et un const char* peuvent etre concatenes avec +.")
 {
-    REQUIRE(std::string { "turlututu" } + "toto" == "turlutututoto");
-    REQUIRE("toto" + std::string { "turlututu" } == "tototurlututu");
+    REQUIRE(std::string{"turlututu"} + "toto" == "turlutututoto");
+    REQUIRE("toto" + std::string{"turlututu"} == "tototurlututu");
 }
 
 TEST_CASE("Test 00 - "
@@ -34,4 +34,3 @@ TEST_CASE("Test 00 - "
     const auto toto = 0;
     REQUIRE(0 + 0 == toto);
 }
-
