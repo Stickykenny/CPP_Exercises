b8056d9218d0d9061e8c06727cdbe3080e6a5626
Jeremy V <gnov.jeremy@gmail.com>
2023-02-09 15:09:08 +0100

    test_commit

--- src/Pokemon.cpp
+++ src/Pokemon.cpp
@@ -1,1 +1,33 @@
 #include "Pokemon.h"
+
+int Pokemon::id_cpt = 1;
+Pokemon::Pokemon(const std::string &name)
+
+    : _name{name}, _id{id_cpt++}
+{
+}
+Pokemon::Pokemon(const Pokemon &other)
+
+    : _name{other._name}, _id{id_cpt++}
+{
+}
+
+const std::string &Pokemon::name() const
+{
+    return Pokemon::_name;
+}
+
+int Pokemon::id() const
+{
+    return Pokemon::_id;
+}
+
+Pokemon &Pokemon::operator=(const Pokemon &other)
+{
+    if (this != &other)
+    {
+        _name = other._name;
+        _id = other._id;
+    }
+    return *this;
+}
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -3,5 +3,18 @@
 #include <string>
 
 // A creature that is cute and can fight other ones.
+
 class Pokemon
-{};
+{
+public:
+    Pokemon(const std::string &name);
+    Pokemon(const Pokemon &other);
+    const std::string &name() const;
+    int id() const;
+    Pokemon &operator=(const Pokemon &other);
+
+private:
+    std::string _name;
+    int _id;
+    static int id_cpt;
+};
