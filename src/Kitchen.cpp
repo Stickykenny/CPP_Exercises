49adf7bbe1aac03af5f631e0c21178a4c0058d60
Jeremy V <gnov.jeremy@gmail.com>
2023-03-14 12:48:59 +0100

    Avancement

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,30 @@
 #include "Kitchen.hpp"
+
+#include "../lib/Unit.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <utility>
+const Unit& Kitchen::register_unit(Unit u)
+{
+    return lst.emplace_back((u));
+
+    // return lst.emplace_back(std::move(u));
+
+    // on renvoie l'élem de la liste
+
+    // move = cast de type
+    //  appel fct de détruire / il est possible qu'il n'est pas détruit
+};
+
+const Unit* Kitchen::find_unit(const std::string& name) const
+{
+    const auto it =
+        std::find_if(lst.begin(), lst.end(), [name](const Unit& unitu) { return unitu.name == name; });
+
+    if (it == lst.end())
+    {
+        return nullptr;
+    }
+    return &(*it); // to obtain Unit* and not the value it points to
+};
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,13 @@
 #pragma once
+#include "../lib/Unit.hpp"
 
+#include <vector>
 class Kitchen
-{};
+{
+public:
+    const Unit& register_unit(Unit u);
+    const Unit* find_unit(const std::string& name) const;
+
+private:
+    std::vector<Unit> lst;
+};
